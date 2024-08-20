afc7bbdd3513433bea3f7191ea8e515b0c1ef866
Takanori Matsumoto <matscube@gmail.com>
2024-08-21 02:50:53 +0900

    feat(node-clack-execa): add README.md with library references feat(node-clack-execa): add @clack/prompts and execa dependencies fix(node-clack-execa): update start script to use ts-node/esm loader feat(node-clack-execa): implement basic functionality using @clack/prompts and execa chore(node-clack-execa): update tsconfig.json to target ES2022 and use NodeNext module

---
+++ node-clack-execa/READMD.md
@@ -0,0 +1,6 @@
+
+### Libraries
+- @clack/prompts
+  - https://github.com/bombshell-dev/clack/tree/main/packages/prompts#readme
+- execa
+  - https://github.com/sindresorhus/execa
--- node-clack-execa/src/index.ts
+++ node-clack-execa/src/index.ts
@@ -1,11 +1,19 @@
-type Test = {
-  a: string;
-  b: number;
-};
+import { intro, outro } from '@clack/prompts';
+import {execa} from 'execa';
 
-const test: Test = {
-  a: 'a',
-  b: 100,
+async function main() {
+  intro(`create-my-app`);
+  
+  // Do stuff
+  // const branch = await execa`git branch --show-current`;
+  // await execa`dep deploy --branch=${branch}`;
+  
+
+  const {stdout} = await execa`npm run build`;
+// Print command's output
+console.log(stdout);
+  
+  outro(`You're all set!`);
 }
 
-console.log(test);
+main();
