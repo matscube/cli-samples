29cd710ef1a9852bfc04bb3fc69efc7f9e64c314
Takanori Matsumoto <matscube@gmail.com>
2024-08-22 18:49:17 +0900

    refactor(index.ts): format import statement for execa to improve readability docs(index.ts): add detailed comments explaining execa command options and behavior

--- node-clack-execa/src/index.ts
+++ node-clack-execa/src/index.ts
@@ -1,19 +1,26 @@
 import { intro, outro } from '@clack/prompts';
-import {execa} from 'execa';
+import { execa } from 'execa';
 
 async function main() {
   intro(`create-my-app`);
-  
-  // Do stuff
-  // const branch = await execa`git branch --show-current`;
-  // await execa`dep deploy --branch=${branch}`;
-  
 
-  const {stdout} = await execa`npm run build`;
-// Print command's output
-console.log(stdout);
-  
+  /**
+   * Between intro and outro, console.log is not output
+   * If stdout is set to 'inherit', the stdout of execa command is displayed to the console.
+   * If stdout is set to 'pipe', the stdout log of execa command can be obtained as the returned value of execa => stdout can be stored as a variable
+   * If stdout is set to 'inherit' and 'pipe', the result of execa command is displayed and the stdout log for execa command is stored as a variable
+   *
+   * If stdin is set, you can pass the value you entered to the child process of execa command
+   */
+  const { stdout, stderr } = await execa({
+    stdout: ['inherit', 'pipe'],
+    stderr: ['inherit', 'pipe'],
+    stdin: 'inherit',
+  })`ssh-keygen -f ./test-key`;
+
   outro(`You're all set!`);
+
+  console.log({ stdout, stderr });
 }
 
 main();
